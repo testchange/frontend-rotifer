c28c8ab1298fb9c557984b3e7be9a647ce4ab93c
Kai <tankaiwei@gmail.com>
2019-12-09 12:20:20 +0800

    change readme

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <file-upload />
+  </div>
+</template>
+
+<script>
+import FileUpload from './components/FileUpload.vue'
+
+export default {
+  name: 'app',
+  components: {
+    FileUpload
+  }
+}
+</script>
+
